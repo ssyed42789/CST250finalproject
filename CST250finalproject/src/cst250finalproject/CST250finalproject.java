af2912fe02571d48cf05c1e33d955492b370a134
Syed Sami <ssyed427890@gmail.com>
2021-05-03 23:29:00 -0500

    final project still under buildng

--- CST250finalproject/src/cst250finalproject/CST250finalproject.java
+++ CST250finalproject/src/cst250finalproject/CST250finalproject.java
@@ -11,26 +11,57 @@ public class CST250finalproject {
         String name;
         int numberShares;
         double buyPrice, sellPrice;
+        
+        Scanner user_input = new Scanner(System.in);
+        
+        String first_name;
+        System.out.print("First Name");
+        first_name = user_input.next();
+        
+        String last_name;
+        System.out.print("Last Name");
+        last_name = user_input.next();
+        
+        String full_name;
+        full_name = first_name + "" + last_name;
+        
+        System.out.println("Your name is verified, now you can move on towards the next step thank you!");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
         String[] shares = new String[] { "Jp Morgan Chase", "Walmart" , "Gamestop","Amc Entertainment","Shell" ,"Google", "Twitter", "Samsung", "Lenovo", "Intel", "Bitcoin", "Litecoin", "SunPower", "American Airlines", "Dave and Buster", "Hecla Mining", "Tilray", "Dogecoin", "Boeing", "Nikola", "Facebook", "British Petroleum", "Bank Of America"  };
 
         System.out.println("Going trough the list: ");
 
-        for (int i = 0; i < shares.length; i++) {
-            System.out.println(shares[i]);
+        for (String share : shares) {
+            System.out.println(share);
         }
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Which share would you like to purchase?");
-        name = input.nextLine();
-
-        System.out.print("How many shares bought?");
-        numberShares = input.nextInt();
-        System.out.print("Buy price?");
-        buyPrice = input.nextDouble();
-        System.out.print("Sale price?");
-        sellPrice = input.nextDouble();
-        input.close();
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Which share would you like to purchase?");
+            name = input.nextLine();
+            
+            System.out.print("How many shares bought?");
+            numberShares = input.nextInt();
+            System.out.print("Buy price?");
+            buyPrice = input.nextDouble();
+            System.out.print("Sale price?");
+            sellPrice = input.nextDouble();
+        }
+        
+        
+       
+            
 
         System.out.println(name + "here is the information of your stock transactions:");
         System.out.println("Number of shares:" + numberShares);
@@ -38,10 +69,29 @@ public class CST250finalproject {
         System.out.println("Amount of sell:" + sellPrice * numberShares);
         System.out.println("Transaction fee paid:" + 15 + 15);
         System.out.println("Net profit:" + (sellPrice * numberShares - buyPrice));
-
+        System.out.println("Net loss:" + (buyPrice - sellPrice ));
+        
+        
+        
     }
-
 }
+
+            
+        
+
+    
+
+
+  
+  
+
+
+        
+    
+   
+   
+
+
         
     
     
